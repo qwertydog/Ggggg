ca6f4ea509a342f6fa1e5ec097e1ba40c3f363d0
qwertydog <campbelldeas@hotmail.com>
2016-05-02 18:01:49 +1200

    Decoder working

---
+++ Ggggg/Ggggg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ggggg
+{
+    internal class Ggggg
+    {
+        Dictionary<char, string> letters;
+
+        public Ggggg(Dictionary<char, string> letters)
+        {
+            this.letters = letters;
+        }
+
+        public string Decode(string input)
+        {
+            string output = string.Empty;
+            string currentLetter = string.Empty;
+
+            for (int i=0; i<input.Length; i++)
+            {
+                currentLetter += input[i].ToString();
+
+                if (!currentLetter.ToUpper().Contains("G"))
+                {
+                    output += currentLetter;
+                    currentLetter = string.Empty;
+                    continue;
+                }
+
+                foreach (var letter in letters)
+                {
+                    if (letter.Value == currentLetter)
+                    {
+                        output += letter.Key;
+                        currentLetter = string.Empty;
+                        break;
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
--- Ggggg/Program.cs
+++ Ggggg/Program.cs
@@ -10,6 +10,23 @@ namespace Ggggg
     {
         static void Main(string[] args)
         {
+            string[] letters = Console.ReadLine().Split(' ');
+            Dictionary<char, string> dict = new Dictionary<char, string>();
+            
+            for (int i=0; i<letters.Length; i+=2)
+            {
+                char letter = char.Parse(letters[i]);
+                string translation = letters[i + 1];
+
+                dict.Add(letter, translation);
+            }
+
+            Ggggg ggggg = new Ggggg(dict);
+
+            string output = ggggg.Decode(Console.ReadLine());
+
+            Console.WriteLine(output);
+            Console.ReadKey();
         }
     }
 }
